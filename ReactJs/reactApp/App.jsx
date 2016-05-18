704f3a41518c0822dccb389437a4988935567b08
hanxue <sine_web@163.com>
2016-05-18 10:54:53 +0800

    react test

--- ReactJs/reactApp/App.jsx
+++ ReactJs/reactApp/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 var HelloMessage = React.createClass({
     render: function() {
-        return <h1>Hello {this.props.name}</h1>;
+        return (<h1>Hello {this.props.name}</h1>);
+    }
+});
+
+var HiSine = React.createClass({
+    render: function() {
+        return (<h1>Hello {this.props.name}</h1>);
     }
 });
 
 class App extends React.Component {
     render() {
         return (
+            <HiSine name="Sine"/>,
             <HelloMessage name="Runoob" />
         );
     }
